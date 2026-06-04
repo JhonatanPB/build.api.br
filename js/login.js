ac82f821ae0225b0499be03bc48f40fa0437220d
JhonatanPB <felixjhonatan250886p@gmail.com>
2026-06-04 03:30:49 -0300

    atualizando a dash

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ function validarLogin() {
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
 
-    if (email === "PB@gmail.com" || senha === "1234") {
+    if (email === "PB@gmail.com" || senha === "13579") {
         window.location.href = "dashboard.html";
         alert("Preencha todos os campos.");
         return false;
